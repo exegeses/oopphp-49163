fd1ab0ca4cb5118056a3455cf458c0bc6e357bd8
Marcos <exegeses@gmail.com>
2021-08-10 19:32:14 -0300

    repaso declaracion de variables, constantes y funciones

---
+++ clase1/repaso.php
@@ -0,0 +1,12 @@
+<?php
+
+    $x = 5;
+    const NOMBRE = 'marcos';
+    //define('NOMBRE', 'marcos');
+    function foo()
+    {
+        return NOMBRE;
+    }
+
+    echo foo();
+    //echo NOMBRE;
