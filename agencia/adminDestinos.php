9ca24a6a70245badf8f6a456992ccab9cf3008b1
Marcos <exegeses@gmail.com>
2021-08-24 19:54:02 -0300

    Pnel de adm,inistración de destinos

--- agencia/adminDestinos.php
+++ agencia/adminDestinos.php
@@ -1,5 +1,9 @@
 <?php
-//    require 'config/config.php';
+    require 'config/config.php';
+    require 'clases/Conexion.php';
+    require 'clases/Destino.php';
+    $Destino = new Destino;
+    $destinos = $Destino->listarDestinos();
     include 'includes/over-all-header.html';
     include 'includes/nav.php';
 ?>
@@ -24,26 +28,30 @@
             </tr>
             </thead>
             <tbody>
-
+        <?php
+            foreach ( $destinos as $destino ){
+        ?>
             <tr>
-                <td>id</td>
-                <td>nombre</td>
-                <td>region</td>
-                <td>$percio</td>
-                <td>asientos</td>
-                <td>disponibles</td>
+                <td><?= $destino['destID'] ?></td>
+                <td><?= $destino['destNombre'] ?></td>
+                <td><?= $destino['regNombre'] ?></td>
+                <td>$<?= $destino['destPrecio'] ?></td>
+                <td><?= $destino['destAsientos'] ?></td>
+                <td><?= $destino['destDisponibles'] ?></td>
                 <td>
-                    <a href="formModificarDestino.php" class="btn btn-outline-secondary">
+                    <a href="formModificarDestino.php?destID=<?= $destino['destID'] ?>" class="btn btn-outline-secondary">
                         Modificar <i class="far fa-edit ml-1"></i>
                     </a>
                 </td>
                 <td>
-                    <a href="formEliminarDestino.php" class="btn btn-outline-secondary">
+                    <a href="formEliminarDestino.php?destID=<?= $destino['destID'] ?>" class="btn btn-outline-secondary">
                         Eliminar <i class="far fa-minus-square ml-1"></i>
                     </a>
                 </td>
             </tr>
-
+        <?php
+            }
+        ?>
             </tbody>
         </table>
 
--- agencia/clases/Destino.php
+++ agencia/clases/Destino.php
@@ -2,5 +2,35 @@
 
     class Destino
     {
+        private $destID;
+        private $destNombre;
+        private $regID;
+        private $destPrecio;
+        private $destAsientos;
+        private $destDisponibles;
+        private $destActivo;
 
+        public function listarDestinos()
+        {
+            $link = Conexion::conectar();
+            /* table relation
+            $sql = "SELECT
+                        destID, destNombre, 
+                        d.regID, regNombre,
+                        destPrecio, destAsientos, destDisponibles
+                     FROM destinos d, regiones r
+                     WHERE d.regID = r.regID";
+            */
+            $sql = "SELECT destID, destNombre, destPrecio, 
+                           destAsientos, destDisponibles, 
+                           destActivo, 
+                           r.regID, r.regNombre 
+                        FROM destinos d 
+                        INNER JOIN regiones r 
+                        ON d.regID = r.regID";
+            $stmt = $link->prepare($sql);
+            $stmt->execute();
+            $destinos = $stmt->fetchAll();
+            return $destinos;
+        }
     }
